dd29b90a39ca0f239697ee70e031265e0d0b5b52
Sivanush <sivanushedapal@gmail.com>
2024-10-26 02:19:33 +0530

    fixing the cors problem

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,12 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { IoAdapter } from '@nestjs/platform-socket.io';
 import { ValidationPipe } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const configService = app.get(ConfigService);
+  const corsOrigin = configService.get<string>('CLIENT_DOMAIN') || 'http://localhost:4200';
   app.enableCors({ origin: 'http://localhost:4200' });
   app.useWebSocketAdapter(new IoAdapter(app))
   app.useGlobalPipes(new ValidationPipe()),

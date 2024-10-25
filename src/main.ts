70a80bdb08b5ed3cabe6c043d166f4a2695afd2e
Sivanush <sivanushedapal@gmail.com>
2024-10-26 02:20:13 +0530

    CORS Updated

--- src/main.ts
+++ src/main.ts
@@ -8,7 +8,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const configService = app.get(ConfigService);
   const corsOrigin = configService.get<string>('CLIENT_DOMAIN') || 'http://localhost:4200';
-  app.enableCors({ origin: 'http://localhost:4200' });
+  app.enableCors({ origin: corsOrigin });
   app.useWebSocketAdapter(new IoAdapter(app))
   app.useGlobalPipes(new ValidationPipe()),
   app.setGlobalPrefix('api')
